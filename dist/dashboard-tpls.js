347b26ba2e1c91a694fd2301142adfd119b7c733
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-05-29 00:03:53 +0200

    Changed template to support item.type. Version bump.

--- dist/dashboard-tpls.js
+++ dist/dashboard-tpls.js
@@ -107,16 +107,23 @@ module.run(['$templateCache', function($templateCache) {
     '	<div mnd-multi-transclude="before"></div>\n' +
     '	<ul class="nav">\n' +
     '		<li ng-repeat="item in menu.items" ng-click="toggleSubmenu(item)" class="mnd-clickable">\n' +
-    '			<a ng-href="{{item.link}}" ng-click="call(item.action)">\n' +
-    '				<span class="mnd-width-20" ng-if="item.icon">\n' +
-    '					<i class="fa {{item.icon}}"></i>\n' +
-    '				</span>\n' +
+    '\n' +
+    '			<a ng-if="item.type === \'link\'" ng-href="{{item.link}}">\n' +
+    '				{{item.title}}\n' +
+    '			</a>\n' +
+    '\n' +
+    '			<a ng-if="item.type === \'action\'" ng-click="call(item.action)">\n' +
+    '				{{item.title}}\n' +
+    '			</a>\n' +
+    '\n' +
+    '			<a ng-if="item.type === \'submenu\'">\n' +
     '				{{item.title}}\n' +
     '				<span ng-if="isSubmenu(item)" class="pull-right">\n' +
     '					<i ng-if="!item.open" class="fa fa-angle-left"></i>\n' +
     '					<i ng-if="item.open" class="fa fa-angle-down"></i>\n' +
     '				</span>\n' +
     '			</a>\n' +
+    '\n' +
     '			<ul ng-if="isSubmenu(item)" collapse="!item.open" class="nav nav-second-level">\n' +
     '				<li ng-repeat="subitem in item.items">\n' +
     '					<a ng-href="{{subitem.link}}" ng-click="call(subitem.action)">\n' +
